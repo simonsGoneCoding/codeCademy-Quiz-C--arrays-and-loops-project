63a43287cd14231f507b5fe064db36e121d6917d
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-04 10:32:55 +0000

    Create ask questions logic.

--- TrueOrFalse/Program.cs
+++ TrueOrFalse/Program.cs
@@ -6,7 +6,52 @@ namespace TrueOrFalse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Do not edit these lines
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+            // Tools.SetUpInputStream(entry);
+
+            /* ------ Setup Arrays ------ */
+            string[] questions = { "Sun is closest to Earth in January.", "Merkury is the hottest planet in our solar system.", "The universe is 13.8 billion years old." };
+
+            bool[] answers = { true, false, true };
+            bool[] responses = new bool[questions.Length];
+
+            if(questions.Length != answers.Length)
+            {
+                Console.WriteLine("Warning! questions and answers arrays are not equal in length");
+            }
+
+
+            /* ------- Ask Questions ------ */
+            int askingIndex = 0; 
+
+            foreach(string question in questions)
+            {
+                string input;
+                bool isBool;
+                bool inputBool;
+
+                Console.WriteLine(question);
+                Console.WriteLine("true or false?");
+                input = Console.ReadLine();
+                isBool = Boolean.TryParse(input, out inputBool);
+
+                while (!isBool)
+                {
+                    Console.WriteLine("Please respond with 'true' or 'false'");
+                    input = Console.ReadLine();
+                    isBool = Boolean.TryParse(input, out inputBool);
+                }
+
+                responses[askingIndex] = inputBool;
+                askingIndex++;
+            }
+
+            foreach (bool response in responses)
+            {
+                Console.WriteLine(response);
+            }
         }
     }
 }
