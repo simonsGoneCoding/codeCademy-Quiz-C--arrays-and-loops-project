8c404ea4bfb199fc5b48dc6fcb280a4bf60dba36
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-04 09:12:07 +0000

    Initial commit - codeCademy - loops and methods project in c#

---
+++ TrueOrFalse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrueOrFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
